7f9517dfb18e21078371da72472f3cd29771da2e
gogl92 <gogl92@users.noreply.github.com>
2022-12-17 04:34:58 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Inquid\StripePaymentLinks\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Inquid\StripePaymentLinks\StripePaymentLinksServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
